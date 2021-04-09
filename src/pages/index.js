a3b892ba34574486e785444bc6ff07d220364983
Alexandre Sueiro <lvx.xande@gmail.com>
2021-04-09 08:24:59 -0300

    testando job preprod novamente

--- src/pages/index.js
+++ src/pages/index.js
@@ -138,6 +138,7 @@ const IndexPage = () => {
           🎉🎉🎉
         </span>
       </h1>
+      <h2>Trabalhando com pipeline no Gitlab</h2>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
